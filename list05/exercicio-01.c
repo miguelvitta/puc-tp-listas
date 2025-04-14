dcb0c55d08a1ac18861b775b32daa776bcebec08
miguelvitta <miguelvittadev@gmail.com>
2025-04-13 22:58:08 -0300

    feat: :zap: implemented the backbone of the program

--- list05/exercicio-01.c
+++ list05/exercicio-01.c
@@ -1,66 +1,53 @@
-#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-#define SIZE_STRING 10001
-#define SIZE_INT 14
+int* createVector(int size);
+void freeVector(int* vector);
+void readVector(int* vector, int size);
+void printVector(int* vector, int size);
+int* resizeVector(int* vector, int originalSize, int newSize);
 
-int getInt();
-void getString(char* str);
+int main() {
+    printf("Bem-vindo ao programa de manipulacao de vetores dinâmicos!\n"
+           "Este programa executa as seguintes operacoes:\n"
+           "  1. Cria dinamicamente um vetor de inteiros com todas as posicoes inicializadas em 0.\n"
+           "  2. Realiza a leitura dos valores do vetor a partir da entrada do usuario.\n"
+           "  3. Imprime os valores armazenados no vetor.\n"
+           "  4. Permite o redimensionamento do vetor para um novo tamanho, transferindo os elementos existentes; "
+           "caso o novo tamanho seja menor, os elementos excedentes serao descartados.\n"
+           "  5. Libera a memoria alocada para o vetor quando ela nao for mais necessaria.\n\n");
 
-int main(int argc, char* argv[]) {
+    int size, newSize;
+    int *vector = NULL;
 
-    printf("This program has the following functions:\n");
+    printf("Digite o tamanho do vetor: ");
+    scanf("%d", &size);
+    vector = createVector(size);
+
+    freeVector(vector);
 
     return 0;
 }
 
-static size_t removeTrailing(size_t len, char* str) {
-    if (len > 0 && str[len - 1] == '\n') {
-        str[len - 1] = '\0';
-        len--;
-    }
-    return len;
+int* createVector(int size) {
+   
+
+    return NULL;
 }
 
-int getInt() {
-    int n = 0;
-    char buffer[SIZE_INT];
-    if (fgets(buffer, SIZE_INT, stdin) != NULL) {
-        size_t len = strlen(buffer);
-        len = removeTrailing(len, buffer);
-
-        for (size_t i = 0; i < len; i++) {
-            if (!isdigit(buffer[i])) {
-                fprintf(stderr, "\nThis is not a valid number.\n");
-                return -1;
-            }
-            n = n * 10 + (buffer[i] - '0');
-            if (n < 0) {
-                fprintf(stderr, "\nNumber is too large.\n");
-                return -2;
-            }
-        }
-    }
-    return n;
+void freeVector(int* vector) {
+   
 }
 
-void getString(char* str) {
-    int valid = 0;
-    while (!valid) {
-        printf("Enter the string: ");
-        if (fgets(str, SIZE_STRING, stdin) != NULL) {
-            size_t len = strlen(str);
-            len = removeTrailing(len, str);
-            if (len > 0) {
-                valid = 1;
-            } else {
-                fprintf(stderr, "\nInput cannot be empty. Try again: ");
-            }
-        } else {
-            fprintf(stderr, "\nError reading the string.\n");
-        }
-    }
+void readVector(int* vector, int size) {
+  
 }
 
+void printVector(int* vector, int size) {
+    
+}
+
+int* resizeVector(int* vector, int originalSize, int newSize) {
+    
+    return NULL;
+}
