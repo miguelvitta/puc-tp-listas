8a65e7d14243e0a2d326bf3534332ae75a652249
miguelvitta <miguelvittadev@gmail.com>
2025-04-14 21:34:18 -0300

    feat: :zap: exercicio 1 is almost ready

--- list05/exercicio-01.c
+++ list05/exercicio-01.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 int* createVector(int size);
-void freeVector(int* vector);
-void readVector(int* vector, int size);
 void printVector(int* vector, int size);
-int* resizeVector(int* vector, int originalSize, int newSize);
+void readVector(int* vector, int size);
+int* resizeVector(int* vector, int newSize);
+void freeVector(int* vector);
 
 int main() {
     printf("Bem-vindo ao programa de manipulacao de vetores dinâmicos!\n"
@@ -17,37 +17,65 @@ int main() {
            "caso o novo tamanho seja menor, os elementos excedentes serao descartados.\n"
            "  5. Libera a memoria alocada para o vetor quando ela nao for mais necessaria.\n\n");
 
-    int size, newSize;
-    int *vector = NULL;
-
-    printf("Digite o tamanho do vetor: ");
+    int size = 0;
+    printf("Write the size of the vector: ");
     scanf("%d", &size);
+
+    int *vector = NULL;
     vector = createVector(size);
 
-    freeVector(vector);
+    printVector(vector, size);
+    readVector(vector, size);
+    printVector(vector, size);
+
+    int newSize = 0;
+    printf("Write the new size of the vector: ");
+    scanf("%d", &newSize);
+
+    int* newVector = NULL;
+    newVector = resizeVector(vector, newSize);
+    printVector(newVector, newSize);
+
+    freeVector(newVector);
 
     return 0;
 }
 
 int* createVector(int size) {
-   
+    int* vector = NULL;
+    vector = malloc(sizeof(vector) * size);
+    for (int i = 0; i < size; i++) {
+        vector[i] = 0;
+    }
 
-    return NULL;
-}
-
-void freeVector(int* vector) {
-   
-}
-
-void readVector(int* vector, int size) {
-  
+    return vector;
 }
 
 void printVector(int* vector, int size) {
-    
+    for (int i = 0; i < size; i++) {
+        printf("vector [%d] = %d\n", i, vector[i]);
+    }      
 }
 
-int* resizeVector(int* vector, int originalSize, int newSize) {
-    
-    return NULL;
+void readVector(int* vector, int size) {
+    printf("Write new values to the vector:");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &vector[i]);
+    }
+}
+
+int* resizeVector(int* vector, int newSize) {
+    int* newVector = NULL;
+    newVector = malloc(sizeof(newVector) * newSize);
+
+    for (int i = 0; i < newSize; i++) {
+        newVector[i] = vector[i];
+    }
+
+    freeVector(vector);
+    return newVector;
+}
+
+void freeVector(int* vector) {
+   free(vector);
 }
