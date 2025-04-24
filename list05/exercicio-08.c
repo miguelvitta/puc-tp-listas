70b11e116a8d108d837190fa4d176879b700b2e5
miguelvitta <miguelvittadev@gmail.com>
2025-04-24 16:20:22 -0300

    feat: solves the exercicio 8 problem

---
+++ list05/exercicio-08.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* findDifference(int* vector1, int size1, int* vector2, int size2, int* size3);
+
+int main() {
+    printf(
+        "This program receives two vectors of integers and creates a new "
+        "vector with the elements that are in the first but not in the second vector.\n");
+
+    int size1 = 0;
+    printf("Write the size of the first vector: \n");
+    scanf("%d", &size1);
+
+    int* vector1 = NULL;
+    vector1 = malloc(sizeof(int) * size1);
+    printf("Write the elements of the first vector: \n");
+    for (int i = 0; i < size1; i++) {
+        scanf("%d", &vector1[i]);
+    }
+
+    int size2 = 0;
+    printf("Write the size of the second vector: \n");
+    scanf("%d", &size2);
+
+    int* vector2 = NULL;
+    vector2 = malloc(sizeof(int) * size2);
+    printf("Write the elements of the second vector: \n");
+    for (int i = 0; i < size2; i++) {
+        scanf("%d", &vector2[i]);
+    }
+
+    int size3 = 0;
+
+    int* vector3 = findDifference(vector1, size1, vector2, size2, &size3);
+
+    printf("The elements of the difference are: \n");
+    for (int i = 0; i < size3; i++) {
+        printf("%d\n", vector3[i]);
+    }
+
+    free(vector1);
+    free(vector2);
+    free(vector3);
+
+    return 0;
+}
+
+int* findDifference(int* vector1, int size1, int* vector2, int size2, int* size3) {
+    int *temp = NULL;
+    temp = malloc(sizeof(int) * size1);
+    int count = 0;
+
+    for (int i = 0; i < size1; i++) {
+        int state = 0;
+        int j = 0;
+        for (; j < size2 && state == 0; j++) {
+            if (vector1[i] == vector2[j]) {
+                state = 1;
+            }
+        }
+        if (j == size2) {
+            temp[count] = vector1[i];
+            count++;
+        }
+    }
+    *size3 = count;
+
+    int* vector3 = NULL;
+    vector3 = malloc(sizeof(int) * count);
+    for (int i = 0; i < count; i++) {
+        vector3[i] = temp[i];
+    }
+    free(temp);
+
+    return vector3;
+}
