a1a3994c86e905d945671c6f57432ac64fde1e04
miguelvitta <miguelvittadev@gmail.com>
2025-04-28 20:30:30 -0300

    feat: Creates the scaffolding of the first program

---
+++ list06/exercicio-01.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int** createMatrix(int numRows, int numCols);
+void readMatrix(int** matrix, int numRows, int numCols);
+void printMatrix(int** matrix, int numRows, int numCols);
+void freeMatrix(int** matrix, int numRows);
+
+int main() {
+    printf("This program performs the following operations:\n"
+           "  1. Dynamically creates an integer matrix.\n"
+           "  2. Reads values into the matrix from user input.\n"
+           "  3. Prints the values stored in the matrix.\n"
+           "  4. Frees the memory allocated for the matrix when it's no longer needed.\n\n");
+
+
+    return 0;
+}
+
+int** createMatrix(int numRows, int numCols) {
+
+}
+
+void readMatrix(int** matrix, int numRows, int numCols) {
+
+}
+
+void printMatrix(int** matrix, int numRows, int numCols) {
+
+}
+
+void freeMatrix(int** matrix, int numRows) {
+
+}
