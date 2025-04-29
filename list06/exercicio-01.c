3703e02f2f1941ebccb9f6912a5212fb4a16ee1c
miguelvitta <miguelvittadev@gmail.com>
2025-04-28 21:27:00 -0300

    feat: :sparkles: Implements the 4 functions needed for exercicio 01

--- list06/exercicio-01.c
+++ list06/exercicio-01.c
@@ -7,23 +7,48 @@ void printMatrix(int** matrix, int numRows, int numCols);
 void freeMatrix(int** matrix, int numRows);
 
 int main() {
-    printf("This program performs the following operations:\n"
-           "  1. Dynamically creates an integer matrix.\n"
-           "  2. Reads values into the matrix from user input.\n"
-           "  3. Prints the values stored in the matrix.\n"
-           "  4. Frees the memory allocated for the matrix when it's no longer needed.\n\n");
+    printf(
+        "This program performs the following operations:\n"
+        "  1. Dynamically creates an integer matrix.\n"
+        "  2. Reads values into the matrix from user input.\n"
+        "  3. Prints the values stored in the matrix.\n"
+        "  4. Frees the memory allocated for the matrix when it's no longer "
+        "needed.\n");
 
+    int numRows = 0;
+    printf("Write the number of rows:\n");
+    scanf("%d", &numRows);
 
+    int numCols = 0;
+    printf("Write the number of columns:\n");
+    scanf("%d", &numCols);
+
+    int** matrix = NULL;
+    matrix = createMatrix(numRows, numCols);
+
+    readMatrix(matrix, numRows, numCols);
+
+    printMatrix(matrix, numRows, numCols);
+
+    freeMatrix(matrix, numRows);
     return 0;
 }
 
 int** createMatrix(int numRows, int numCols) {
+    int** matrix = NULL;
+    matrix = malloc(sizeof(int*) * numRows);
 
+    for (int i = 0; i < numRows; i++) {
+        matrix[i] = malloc(sizeof(int) * numCols);
+    }
+
+    return matrix;
 }
 
 void readMatrix(int** matrix, int numRows, int numCols) {
     for (int i = 0; i < numRows; i++) {
         for (int j = 0; j < numCols; j++) {
+            printf("Enter element [%d][%d]: ", i, j);
             scanf("%d", &matrix[i][j]);
         }
     }
