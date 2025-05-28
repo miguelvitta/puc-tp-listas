6e698768ae2d27705f8d4115bf359e367fbb68fc
miguelvitta <miguelvittadev@gmail.com>
2025-05-28 18:06:25 -0300

    feat: Exercise 4 is mostly done

--- list07/exercicio-04.c
+++ list07/exercicio-04.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -55,18 +56,79 @@ int main() {
     return 0;
 }
 
-void togglePower(tv* t) {}
+void togglePower(tv* t) { t->status = t->status == 1 ? 0 : 1; }
 
-int isTvOn(tv* t) {}
+int isTvOn(tv* t) { return t->status; }
 
-int currentChannelNumber(tv* t) {}
+int currentChannelNumber(tv* t) {
+    if (t->status == 1) {
+        return t->currentChannelNumber;
+    }
+    return -1;
+}
 
-const char* currentChannelName(tv* t) {}
+const char* currentChannelName(tv* t) {
+    if (t->status == 1) {
+        for (int i = 0; i < t->totalChannels; i++) {
+            if (t->currentChannelNumber == t->canais[i].channelNumber) {
+                return t->canais[i].channelName;
+            }
+        }
+        return "No signal.\n";
+    }
+    return "TV is off.\n";
+}
 
-void nextChannel(tv* t) {}
+void nextChannel(tv* t) {
+    if (t->status != 1) {
+        return;
+    }
+    for (int i = 0; i < t->totalChannels; i++) {
+        if (t->canais[i].channelNumber > t->currentChannelNumber) {
+            t->currentChannelNumber = t->canais[i].channelNumber;
+            return;
+        }
+    }
+    t->currentChannelNumber = t->canais[0].channelNumber;
+}
 
-void previousChannel(tv* t) {}
+void previousChannel(tv* t) {
+    if (t->status != 1) {
+        return;
+    }
+    for (int i = t->totalChannels - 1; i >= 0; i--) {
+        if (t->canais[i].channelNumber < t->currentChannelNumber) {
+            t->currentChannelNumber = t->canais[i].channelNumber;
+            return;
+        }
+    }
+    t->currentChannelNumber = t->canais[t->totalChannels - 1].channelNumber;
+}
 
-int goToChannel(tv* t, int channelNumber) {}
+int goToChannel(tv* t, int channelNumber) {
+    if (t->status == 1) {
+        t->currentChannelNumber = channelNumber;
+        for (int i = 0; i < t->totalChannels; i++) {
+            if (t->canais[i].channelNumber == t->currentChannelNumber) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+    return 0;
+}
 
-void printTv(tv* t) {}
+void printTv(tv* t) {
+    if (t->status == 1) {
+        for (int i = 0; i < t->totalChannels; i++) {
+            if (t->canais[i].channelNumber == t->currentChannelNumber) {
+                printf("%d - %s\n", t->canais[i].channelNumber,
+                       t->canais[i].channelName);
+                return;
+            }
+        }
+        printf("No signal.\n");
+        return;
+    }
+    printf("TV is off.\n");
+}
